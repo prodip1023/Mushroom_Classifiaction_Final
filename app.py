fd8a329a140cccc42ef8659d540dcb5e865d45f4
prodip1023 <prodip1023@gmail.com>
2024-03-31 01:34:54 +0530

    completed logger file

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 # import the Flask 
 from flask import Flask
+from mushroom.logger import logging
 
 # create the application object
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    logging.info('A request was made to the home page')
     return 'This is My New Application,Name - Mushroom Classification'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
