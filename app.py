ad29e024ec59b97d1da163ac0a874be6e232d4fa
prodip1023 <prodip1023@gmail.com>
2024-04-01 22:58:28 +0530

    exception file added

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 # import the Flask 
+import sys
 from flask import Flask
 from mushroom.logger import logging
+from mushroom.exception import MushroomException
 
 # create the application object
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    logging.info('A request was made to the home page')
+    try:
+        raise Exception("We are testing custom Exception")
+    except Exception as e:
+        mushroom = MushroomException(e,sys)
+        logging.info(mushroom.error_message)
+        logging.info('A request was made to the home page')
     return 'This is My New Application,Name - Mushroom Classification'
 
 if __name__ == '__main__':
